ed20b8e4dadce3217a336ebc06786dd526a0c0c6
gamal haroun <boxcity0@gmail.com>
2024-11-25 11:02:02 +0200

    create category module

---
+++ apps/admin/src/app/category/category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-category',
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss'
+})
+export class CategoryComponent {
+
+}
