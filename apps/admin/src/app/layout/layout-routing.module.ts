240f237dd962b73c9849c7fbb31f1379cb69fa89
gamal haroun <boxcity0@gmail.com>
2024-12-02 23:49:22 +0200

    implement auth guard

--- apps/admin/src/app/layout/layout-routing.module.ts
+++ apps/admin/src/app/layout/layout-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { SecuredLayoutComponent } from './secured-layout/secured-layout.component';
+import { AuthGuard } from '@e-shop/auth';
 
 const routes: Routes = [
   {
     path: '',
     component: SecuredLayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'category',
