aff1fb885dfe0bc398abbaaabc67c659f38d2ec6
gamal haroun <boxcity0@gmail.com>
2024-12-02 23:49:55 +0200

    add wild card for routes

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
+    path: 'dashboard',
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/dashboard',
   },
 ];
