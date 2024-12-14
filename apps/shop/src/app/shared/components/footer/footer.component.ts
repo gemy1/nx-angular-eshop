4e7deefeea2f747975a7419bf1a4c2b34b59a57e
gamal haroun <boxcity0@gmail.com>
2024-12-14 14:26:58 +0200

    add layout module to shop

--- apps/shop/src/app/shared/components/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'shop-footer',
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
-})
-export class FooterComponent {}
